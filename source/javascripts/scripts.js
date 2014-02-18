a511291548f50ae17cb7d03d8e4f0447f3d40f4b
alauderdale <alauderdale@mac.com>
2014-02-17 22:16:15 -0700

    navbar

---
+++ source/javascripts/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+	//fancybox
+	$(".fancybox").fancybox();
+	
+	$('.fb-video').fancybox({
+        padding: 0,
+        helpers: {
+            media: {}
+        }
+    });
+
+});
