aa16659cdf9a3a852f475a59507be30d58f96657
Alex Lauderdale <alauderdale@mac.com>
2014-02-26 19:23:30 -0700

    btn

--- source/javascripts/scripts.js
+++ source/javascripts/scripts.js
@@ -1,16 +1,21 @@
 $(document).ready(function(){
 
-	//fancybox
-	$(".fancybox").fancybox({
-		openEffect: "elastic",
-		closeEffect: "elastic"
+////Jquery Datatable setup
+
+	////toggle the filter button
+	$('.filter-toggle').click(function() {
+		$('.wh-filter-inner').toggleClass('hidden');
 	});
-	
-	$('.fb-video').fancybox({
-        padding: 0,
-        helpers: {
-            media: {}
-        }
-    });
+
+	////clear the filter
+	$("#clear-filter").click(function(e) {
+		
+	});
+
+	////Set Table defaults
+	$.extend( $.fn.dataTable.defaults, {
+		"bLengthChange": false
+	} );
+
 
 });
